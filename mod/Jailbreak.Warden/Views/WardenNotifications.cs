1a4fd132878c74cd3313dd5238c48369598ce8cb
Mooshua <43320783+mooshua@users.noreply.github.com>
2024-01-24 11:55:59 -0800

    Change pre-made notification auto-properties to fields.

--- mod/Jailbreak.Warden/Views/WardenNotifications.cs
+++ mod/Jailbreak.Warden/Views/WardenNotifications.cs
@@ -16,28 +16,28 @@ public static class WardenNotifications
 		Chat = true,
 	};
 
-	public static IView PICKING_SHORTLY => new SimpleView(writer =>
+	public static IView PICKING_SHORTLY = new SimpleView(writer =>
 		writer
 			.Line(PREFIX, "Picking a warden shortly")
 			.Line(PREFIX, "To enter the warden queue, type !warden in chat."));
 
-	public static IView NO_WARDENS => new SimpleView(writer =>
+	public static IView NO_WARDENS = new SimpleView(writer =>
 		writer
 			.Line(PREFIX, "No wardens in queue! The next player to run !warden will become a warden."));
 
-	public static IView WARDEN_LEFT => new SimpleView(writer =>
+	public static IView WARDEN_LEFT = new SimpleView(writer =>
 		writer.Line(PREFIX, "The warden has left the game!"));
 
-	public static IView WARDEN_DIED => new SimpleView(writer =>
+	public static IView WARDEN_DIED = new SimpleView(writer =>
 		writer.Line(PREFIX, "The warden has died!"));
 
-	public static IView BECOME_NEXT_WARDEN => new SimpleView(writer =>
+	public static IView BECOME_NEXT_WARDEN = new SimpleView(writer =>
 		writer.Line(PREFIX, "Type !warden to become the next warden"));
 
-	public static IView JOIN_RAFFLE => new SimpleView(writer =>
+	public static IView JOIN_RAFFLE = new SimpleView(writer =>
 		writer.Line(PREFIX, "You've joined the warden raffle!"));
 
-	public static IView LEAVE_RAFFLE => new SimpleView(writer =>
+	public static IView LEAVE_RAFFLE = new SimpleView(writer =>
 		writer.Line(PREFIX, "You've left the warden raffle!"));
 
 }
