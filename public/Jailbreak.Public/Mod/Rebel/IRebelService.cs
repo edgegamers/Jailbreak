4986723510f0b65c0a6cf7d6fd75ff14184ba56e
MSWS <imodmaker@gmail.com>
2024-02-02 22:57:34 -0800

    Add rebel module

---
+++ public/Jailbreak.Public/Mod/Rebel/IRebelService.cs
@@ -0,0 +1,19 @@
+﻿using CounterStrikeSharp.API.Core;
+
+namespace Jailbreak.Public.Mod.Rebel;
+
+public interface IRebelService
+{
+    ISet<CCSPlayerController> GetActiveRebels();
+
+    bool IsRebel(CCSPlayerController player)
+    {
+        return GetRebelTimeLeft(player) > 0;
+    }
+
+    float GetRebelTimeLeft(CCSPlayerController player);
+
+    bool MarkRebel(CCSPlayerController player, float time);
+    
+    void UnmarkRebel(CCSPlayerController player);
+}
