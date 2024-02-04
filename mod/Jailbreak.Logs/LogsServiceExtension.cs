ff1a930b16cdc689fa694fdf4ffeb9718c409b68
MSWS <imodmaker@gmail.com>
2024-02-03 23:28:30 -0800

    merge logs

---
+++ mod/Jailbreak.Logs/LogsServiceExtension.cs
@@ -0,0 +1,16 @@
+﻿using Jailbreak.Public.Extensions;
+using Jailbreak.Public.Mod.Logs;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Jailbreak.Logs;
+
+public static class LogsServiceExtension
+{
+   public static void AddLogsService(this IServiceCollection services)
+   {
+      services.AddPluginBehavior<ILogService, LogsManager>();
+      
+      services.AddPluginBehavior<LogsCommand>();
+      services.AddPluginBehavior<LogsListeners>();
+   } 
+}
