c04370117e9544289d86f2a39e23bd11c1157c7d
Mooshua <43320783+mooshua@users.noreply.github.com>
2024-02-07 18:40:36 -0800

    Add service extensions

---
+++ mod/Jailbreak.Lasers/MarkerServiceExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Jailbreak.Drawable.Markers;
+using Jailbreak.Public.Extensions;
+using Jailbreak.Public.Mod.Draw;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Jailbreak.Drawable;
+
+public static class MarkerServiceExtensions
+{
+	public static void AddLasers(this IServiceCollection collection)
+	{
+		collection.AddSingleton<IDrawableFactory, DrawableFactory>();
+
+		collection.AddPluginBehavior<IMarkerService, MarkerBehavior>();
+		collection.AddPluginBehavior<MarkerListener>();
+	}
+}
