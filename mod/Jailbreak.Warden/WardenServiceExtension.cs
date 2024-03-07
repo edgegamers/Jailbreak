b00447b3b981d29250b90badf9646d5211cf2bfe
BuildTools <unconfigured@null.spigotmc.org>
2024-03-07 01:57:33 +0000

    fixed merge issue

--- mod/Jailbreak.Warden/WardenServiceExtension.cs
+++ mod/Jailbreak.Warden/WardenServiceExtension.cs
@@ -16,11 +16,7 @@ public static class WardenServiceExtension
     {
         serviceCollection.AddPluginBehavior<IWardenService, WardenBehavior>();
         serviceCollection.AddPluginBehavior<IWardenSelectionService, WardenSelectionBehavior>();
-<<<<<<< Updated upstream
         serviceCollection.AddPluginBehavior<IWardenPeaceService, WardenPeaceBehaviour>();
-=======
-        serviceCollection.AddPluginBehavior<IWardenLastGuardService, WardenLastGuardBehavior>();
->>>>>>> Stashed changes
 
         serviceCollection.AddPluginBehavior<WardenCommandsBehavior>();
         serviceCollection.AddPluginBehavior<WardenMarkerBehavior>();
