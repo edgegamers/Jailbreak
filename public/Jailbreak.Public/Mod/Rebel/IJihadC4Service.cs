6c7689c16d96585c32445a08c57199091a7365a8
BuildTools <unconfigured@null.spigotmc.org>
2024-03-12 03:24:47 +0000

    Todo list/concept idea

---
+++ public/Jailbreak.Public/Mod/Rebel/IJihadC4Service.cs
@@ -0,0 +1,50 @@
+﻿
+using CounterStrikeSharp.API.Core;
+
+namespace Jailbreak.Public.Mod.Rebel;
+
+public interface IJihadC4Service
+{
+
+    /// <summary>
+    /// Calls the GiveJihadC4ToTerrorist function on a randomly chosen Terrorist 
+    /// at the start of each round, subject to certain conditions (such as if there
+    /// are more than 6 terrorists at the start of the round).
+    /// </summary>
+    public void GiveJihadC4ToRandomTerrorist();
+
+    /// <summary>
+    /// Give the "Jihad C4" to the specified Terrorist. This bomb is special and when 
+    /// the +use key is pressed when the bomb is equipped it will blow up and 
+    /// physically blast the Terrorists as well as damage the Guards in a certain radius.
+    /// </summary>
+    /// <param name="terrorist"></param>
+    public void GiveJihadC4ToTerrorist(CCSPlayerController terrorist);
+
+    /// <summary>
+    /// The function that should actually do all the calculations for blowing up the 
+    /// "Jihad C4". This function should also obviously only work if the supplied player
+    /// controller actually has the "Jihad C4" and is holding it out. 
+    /// </summary>
+    /// <param name="terrorist"></param>
+    public void TryBlowUpJihadC4(CCSPlayerController terrorist);
+
+    /// <summary>
+    /// Attempts to defuse the "Jihad C4" by disabling the Terrorist from being able to 
+    /// use the bomb. This should only work if the supplied player controller actually has
+    /// the "Jihad C4" and is holding it out.
+    /// 
+    /// The default command for this should be css_defuse
+    /// </summary>
+    /// <param name="terrorist"></param>
+    public void TryDefuseJihadC4(CCSPlayerController terrorist);
+
+    /// <summary>
+    /// Attempts to set a delay on the "Jihad C4". This means when the bomb is successfully 
+    /// used it should not immediately explode (as is the default) but instead explode after the 
+    /// given delay. 
+    /// </summary>
+    /// <param name="terrorist"></param>
+    public void TrySetDelayJihadC4(CCSPlayerController terrorist, float delay);
+
+}
