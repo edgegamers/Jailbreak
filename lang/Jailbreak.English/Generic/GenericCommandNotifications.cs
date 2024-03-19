6e5d58ffcd12f4b16ba0e8c64525f35c45ac0fad
BuildTools <unconfigured@null.spigotmc.org>
2024-03-19 19:37:17 +0000

    fix

--- lang/Jailbreak.English/Generic/GenericCommandNotifications.cs
+++ lang/Jailbreak.English/Generic/GenericCommandNotifications.cs
@@ -11,7 +11,7 @@ namespace Jailbreak.English.Generic;
 public class GenericCommandNotifications : IGenericCommandNotifications, ILanguage<Formatting.Languages.English>
 {
 	public static FormatObject GENERIC_PREFIX =
-		new HiddenFormatObject($" {ChatColors.DarkRed}[{ChatColors.LightRed}JB{ChatColors.DarkRed}]")
+		new HiddenFormatObject($" {ChatColors.Darkred}[{ChatColors.LightRed}JB{ChatColors.Darkred}]")
 		{
 			//	Hide in panorama and center text
 			Plain = false,
