8c321078701867553c3b421b20c37472d480b9bb
BuildTools <unconfigured@null.spigotmc.org>
2024-03-29 10:32:12 +0100

    Add weapon drops to logs

--- mod/Jailbreak.Logs/Listeners/LogEntityListeners.cs
+++ mod/Jailbreak.Logs/Listeners/LogEntityListeners.cs
@@ -1,7 +1,8 @@
 ﻿using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
-
+using CounterStrikeSharp.API.Modules.Memory.DynamicFunctions;
+using CounterStrikeSharp.API.Modules.Utils;
 using Jailbreak.Formatting.Views;
 using Jailbreak.Public.Behaviors;
 using Jailbreak.Public.Extensions;
@@ -15,6 +16,24 @@ public class LogEntityListeners : IPluginBehavior
 	public LogEntityListeners(IRichLogService logs)
 	{
 		_logs = logs;
+		OnDropGun();
+	}
+	
+	private static MemoryFunctionVoid<CBasePlayerPawn, CBasePlayerWeapon, IntPtr> WeaponDrop = new( "\\x55\\x48\\x89\\xE5\\x41\\x56\\x41\\x55\\x49\\x89\\xD5\\x41\\x54\\x49\\x89\\xFC\\x53\\x48\\x89\\xF3\\xE8\\x2A\\x2A\\x2A\\x2A" );
+
+	private void OnDropGun()
+	{
+		WeaponDrop.Hook(hook =>
+		{
+			var player = hook.GetParam<CBasePlayerPawn>(0);
+			var weapon = hook.GetParam<CBasePlayerWeapon>(1);
+
+			if (!player.IsValid) return HookResult.Continue;
+			if (player.TeamNum != (byte)CsTeam.CounterTerrorist) return HookResult.Changed;
+			
+			_logs.Append(_logs.Player((CCSPlayerController)player.Controller.Value), $"dropped weapon: {weapon.DesignerName}");
+			return HookResult.Continue;
+		}, HookMode.Pre);
 	}
 
 	[EntityOutputHook("func_button", "OnPressed")]
