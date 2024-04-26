86d2e520cd040bac15b23b432a9be7ea6bbb8dbc
BuildTools <unconfigured@null.spigotmc.org>
2024-04-26 21:34:02 +0200

    Add roll command for warden

--- mod/Jailbreak.Warden/WardenServiceExtension.cs
+++ mod/Jailbreak.Warden/WardenServiceExtension.cs
@@ -23,6 +23,7 @@ public static class WardenServiceExtension
         serviceCollection.AddPluginBehavior<SpecialTreatmentCommandsBehavior>();
         serviceCollection.AddPluginBehavior<PeaceCommandsBehavior>();
         serviceCollection.AddPluginBehavior<WardenCommandsBehavior>();
+        serviceCollection.AddPluginBehavior<RollCommandBehavior>();
 
         serviceCollection.AddPluginBehavior<WardenMarkerBehavior>();
         serviceCollection.AddPluginBehavior<WardenPaintBehavior>();
