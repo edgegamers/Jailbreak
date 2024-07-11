e2de9d429f366299f4b6f835e10253f0d98253d7
shook <shookeagle@gmail.com>
2024-07-10 22:29:06 -0500

    cleanup actually this time

--- mod/Jailbreak.Logs/Listeners/LogEntityParentListeners.cs
+++ mod/Jailbreak.Logs/Listeners/LogEntityParentListeners.cs
@@ -44,7 +44,7 @@ public class LogEntityParentListeners(IRichLogService logs) : IPluginBehavior {
     if (!newParent.IsValid) // a.k.a parent is world
     {
       logs.Append(logs.Player(weaponOwner),
-        $"dropped their {weaponEntity.ToFriendlyString}");
+        $"dropped their {weaponEntity.ToFriendlyString()}");
       return;
     }
 
@@ -53,7 +53,10 @@ public class LogEntityParentListeners(IRichLogService logs) : IPluginBehavior {
     ?.OriginalController.Get();
     if (weaponPickerUpper == null) return;
 
-    logs.Append(weaponPickerUpper, "picked up",
-      logs.Player(weaponOwner), $"'s {weaponEntity.ToFriendlyString()}");
+    if (weaponPickerUpper == weaponOwner)
+    {
+        logs.Append(weaponPickerUpper, $"picked up their {weaponEntity.ToFriendlyString()}");
+    }
+    logs.Append(weaponPickerUpper, "picked up", logs.Player(weaponOwner), $"{weaponEntity.ToFriendlyString()}");
   }
 }
