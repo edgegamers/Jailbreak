a58733c03985695709f88a9e95c875bbbbe8211e
MSWS <imodmaker@gmail.com>
2024-07-19 15:08:16 -0700

    Gradually give glocks

--- mod/Jailbreak.LastGuard/LastGuard.cs
+++ mod/Jailbreak.LastGuard/LastGuard.cs
@@ -8,6 +8,7 @@ using Jailbreak.Public.Behaviors;
 using Jailbreak.Public.Extensions;
 using Jailbreak.Public.Mod.LastGuard;
 using Jailbreak.Public.Mod.LastRequest;
+using Microsoft.VisualBasic;
 
 namespace Jailbreak.LastGuard;
 
@@ -15,6 +16,22 @@ public class LastGuard(LastGuardConfig config,
   ILastGuardNotifications notifications, ILastRequestManager lrManager)
   : ILastGuardService, IPluginBehavior {
   private bool canStart;
+  private bool isLastGuard = false;
+  private List<CCSPlayerController> lastGuardPrisoners = new();
+  private readonly Random rng = new();
+
+  private static readonly HashSet<string> NON_WEAPONS = new() {
+    "weapon_knife",
+    "weapon_c4",
+    "weapon_hegrenade",
+    "weapon_flashbang",
+    "weapon_smokegrenade",
+    "weapon_decoy",
+    "weapon_incgrenade",
+    "weapon_healthshot",
+    "weapon_molotov",
+    "weapon_taser"
+  };
 
   public int CalculateHealth() {
     var aliveTerrorists = Utilities.GetPlayers()
@@ -33,6 +50,8 @@ public class LastGuard(LastGuardConfig config,
 
     if (guardPlayerPawn == null || !guardPlayerPawn.IsValid) return;
 
+    isLastGuard = true;
+
     var guardCalcHealth = CalculateHealth();
 
     guardPlayerPawn.Health = guardCalcHealth;
@@ -41,13 +60,13 @@ public class LastGuard(LastGuardConfig config,
     // foreach (var player in Utilities.GetPlayers().Where(p => p.IsReal()))
     //     player.ExecuteClientCommand("play sounds/lastct");
 
-    var aliveTerrorists = Utilities.GetPlayers()
-     .Where(p
-        => p.IsReal() && p is { PawnIsAlive: true, Team: CsTeam.Terrorist })
+    lastGuardPrisoners = Utilities.GetPlayers()
+     .Where(p => p is { PawnIsAlive: true, Team: CsTeam.Terrorist })
      .ToList();
 
     var prisonerHp =
-      aliveTerrorists.Sum(prisoner => prisoner.PlayerPawn?.Value?.Health ?? 0);
+      lastGuardPrisoners.Sum(
+        prisoner => prisoner.PlayerPawn?.Value?.Health ?? 0);
 
     notifications.LGStarted(guardCalcHealth, prisonerHp)
      .ToAllCenter()
@@ -55,14 +74,22 @@ public class LastGuard(LastGuardConfig config,
 
     if (string.IsNullOrEmpty(config.LastGuardWeapon)) return;
 
-    foreach (var player in aliveTerrorists)
+    foreach (var player in lastGuardPrisoners)
       player.GiveNamedItem(config.LastGuardWeapon);
   }
 
   [GameEventHandler]
   public HookResult OnPlayerDeathEvent(EventPlayerDeath @event,
     GameEventInfo info) {
+    var player = @event.Userid;
+    if (player == null) return HookResult.Continue;
     checkLastGuard(@event.Userid);
+
+    if (!isLastGuard) return HookResult.Continue;
+
+    if (player.Team != CsTeam.Terrorist) return HookResult.Continue;
+
+    giveGun(player);
     return HookResult.Continue;
   }
 
@@ -73,8 +100,33 @@ public class LastGuard(LastGuardConfig config,
     return HookResult.Continue;
   }
 
+  private void giveGun(CCSPlayerController poi) {
+    lastGuardPrisoners = lastGuardPrisoners.Where(p
+        => p is { IsValid: true, PawnIsAlive: true } && poi.SteamID != p.SteamID
+        && !playerHasGun(p))
+     .ToList();
+    if (lastGuardPrisoners.Count == 0) { return; }
+
+    var random = lastGuardPrisoners[rng.Next(lastGuardPrisoners.Count)];
+    random.GiveNamedItem("weapon_glock");
+    lastGuardPrisoners.Remove(random);
+  }
+
+  private bool playerHasGun(CCSPlayerController player) {
+    var weapons = player.Pawn.Value?.WeaponServices;
+    if (weapons == null) return false;
+    foreach (var weapon in weapons.MyWeapons) {
+      if (weapon == null || weapon.Value == null) continue;
+      if (NON_WEAPONS.Contains(weapon.Value!.DesignerName)) continue;
+      return true;
+    }
+
+    return false;
+  }
+
   private void checkLastGuard(CCSPlayerController? poi) {
     if (poi == null) return;
+    lastGuardPrisoners.Remove(poi);
     if (poi.Team != CsTeam.CounterTerrorist) return;
     var aliveCts = Utilities.GetPlayers()
      .Count(plr => plr.SteamID != poi.SteamID && plr is {
@@ -88,6 +140,12 @@ public class LastGuard(LastGuardConfig config,
       });
 
     if (canStart) StartLastGuard(lastGuard);
+  }
+
+  [GameEventHandler]
+  public HookResult OnRoundEnd(EventRoundEnd @event, GameEventInfo info) {
+    isLastGuard = false;
+    return HookResult.Continue;
   }
 
   [GameEventHandler]
