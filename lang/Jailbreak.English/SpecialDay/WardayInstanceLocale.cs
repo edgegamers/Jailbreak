8658d719a1fe4001b1f027674a334f47f222f080
MSWS <imodmaker@gmail.com>
2024-08-03 05:40:40 -0700

    Remove unneded {}s

--- lang/Jailbreak.English/SpecialDay/WardayInstanceLocale.cs
+++ lang/Jailbreak.English/SpecialDay/WardayInstanceLocale.cs
@@ -5,8 +5,7 @@ namespace Jailbreak.English.SpecialDay;
 public class WardayInstanceLocale() : TeamDayLocale("Warday",
   "CTs vs Ts! CTs pick a room, Ts must go fight them!",
   "CTs MUST stay in the same room until expansion time.") {
-  public IView ExpandNow
-    => new SimpleView { { PREFIX, "CTs can expand now!" } };
+  public IView ExpandNow => new SimpleView { PREFIX, "CTs can expand now!" };
 
   public IView ExpandIn(int seconds) {
     return new SimpleView {
