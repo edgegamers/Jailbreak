c25591751cb421d03bc34e15abec199de3aa7c17
MSWS <imodmaker@gmail.com>
2024-08-12 19:16:19 -0700

    More strict distance requirement

--- mod/Jailbreak.Trail/ActivePlayerTrail.cs
+++ mod/Jailbreak.Trail/ActivePlayerTrail.cs
@@ -42,7 +42,7 @@ public abstract class ActivePlayerTrail<T> : AbstractTrail<T>
     pos = pos.Clone();
     var end  = GetEndSegment();
     var dist = end?.GetStart().DistanceSquared(pos) ?? float.MaxValue;
-    if (dist < 2000) {
+    if (dist < 1000) {
       // Still want to remove old segments
       Cleanup();
       DidntMoveTicks++;
@@ -51,6 +51,8 @@ public abstract class ActivePlayerTrail<T> : AbstractTrail<T>
     }
 
     DidntMoveTicks = 0;
+    if (dist < 5000) { return; }
+
     AddTrailPoint(pos);
   }
 
