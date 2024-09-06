955bcd79023d266108df5a6373e002ef7e021eb4
MSWS <imodmaker@gmail.com>
2024-09-05 19:44:58 -0700

    Add support for toggline Auto-RTD

---
+++ public/Jailbreak.Formatting/Views/RTD/IAutoRTDLocale.cs
@@ -0,0 +1,8 @@
+﻿using Jailbreak.Formatting.Base;
+
+namespace Jailbreak.Formatting.Views.RTD;
+
+public interface IAutoRTDLocale {
+  public IView TogglingNotEnabled();
+  public IView AutoRTDToggled(bool enabled);
+}
