f0e5824a6c4e7e67b4b4be6e7db424cb79a9b6a0
MSWS <imodmaker@gmail.com>
2024-09-18 20:24:01 -0700

    Dont enable LR if already enabled

--- mod/Jailbreak.LastRequest/LastRequestManager.cs
+++ mod/Jailbreak.LastRequest/LastRequestManager.cs
@@ -253,6 +253,7 @@ public class LastRequestManager(ILRLocale messages, IServiceProvider provider)
 
   private void checkLR() {
     Server.RunOnTick(Server.TickCount + 32, () => {
+      if (IsLREnabled) return;
       if (Utilities.GetPlayers().All(p => p.Team != CsTeam.CounterTerrorist))
         return;
       if (countAlivePrisoners() > CV_PRISONER_TO_LR.Value) return;
