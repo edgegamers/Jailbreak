84c9ec98ba8642b9e4095b01f97fb2b3a7657bf0
MSWS <imodmaker@gmail.com>
2024-10-25 14:54:11 -0700

    Fix spacing again

--- mod/Jailbreak.RTD/Rewards/CreditReward.cs
+++ mod/Jailbreak.RTD/Rewards/CreditReward.cs
@@ -15,7 +15,7 @@ public class CreditReward(int credits) : IRTDReward {
 
   public string Name => $"{credits} credit{(credits == 1 ? "" : "s")}";
 
-  public string Description => $"You won {Name} {(credits > 500 ? "!" : ".")}";
+  public string Description => $"You won {Name}{(credits > 500 ? "!" : ".")}";
 
   public bool Enabled => API.Gangs != null;
 
