272a864d2698bb3649eb6f6153c83fdac3c6f854
ntm <35658945+ntm5@users.noreply.github.com>
2024-12-22 00:17:47 +0100

    Add a cvar for bullet for bullet
    Fixes #322

--- mod/Jailbreak.LastRequest/LastRequests/BulletForBullet.cs
+++ mod/Jailbreak.LastRequest/LastRequests/BulletForBullet.cs
@@ -18,6 +18,9 @@ public class BulletForBullet : TeleportingRequest {
   private readonly bool magForMag;
   private readonly ILRB4BLocale msg;
   private int? whosShot, magSize;
+  public static readonly FakeConVar<bool> KILL_BY_HEALTH = new(
+    "css_jb_lr_b4b_kill_by_health",
+    "If true, the player with the lowest health will die after 60 seconds.");
 
   public BulletForBullet(BasePlugin plugin, IServiceProvider provider,
     CCSPlayerController prisoner, CCSPlayerController guard,
@@ -68,6 +71,9 @@ public class BulletForBullet : TeleportingRequest {
       Prisoner.GiveNamedItem("weapon_knife");
       Guard.GiveNamedItem("weapon_knife");
     });
+
+    if (!KILL_BY_HEALTH.Value) return;
+    
     Plugin.AddTimer(60, () => {
       if (State != LRState.ACTIVE) return;
       var result = Guard.Health > Prisoner.Health ?
