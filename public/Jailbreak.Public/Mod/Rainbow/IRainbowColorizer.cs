4358ebf19dea2fa39638e5da8c863363a3410eb7
MSWS <imodmaker@gmail.com>
2024-12-21 23:36:03 -0800

    Add rainbowizers

---
+++ public/Jailbreak.Public/Mod/Rainbow/IRainbowColorizer.cs
@@ -0,0 +1,15 @@
+﻿using CounterStrikeSharp.API.Core;
+using Jailbreak.Public.Behaviors;
+
+namespace Jailbreak.Public.Mod.Rainbow;
+
+public interface IRainbowColorizer : IPluginBehavior {
+  void StartRainbow(CCSPlayerController player);
+
+  void StopRainbow(CCSPlayerController player) {
+    if (!player.IsValid) return;
+    StopRainbow(player.Slot);
+  }
+
+  void StopRainbow(int slot);
+}
