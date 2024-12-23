9801f591c7deeb07a1beaf0201c8eb1d554b47e9
MSWS <imodmaker@gmail.com>
2024-12-23 03:04:32 -0800

    Revert bomb icon command

--- mod/Gangs.BombIconPerk/BombIconCommand.cs
+++ mod/Gangs.BombIconPerk/BombIconCommand.cs
@@ -1,5 +1,4 @@
-﻿using Gangs.BaseImpl;
-using GangsAPI;
+﻿using GangsAPI;
 using GangsAPI.Data;
 using GangsAPI.Data.Command;
 using GangsAPI.Exceptions;
@@ -16,18 +15,115 @@ using Microsoft.Extensions.Localization;
 
 namespace Gangs.BombIconPerk;
 
-public class BombIconCommand(IServiceProvider provider)
-  : AbstractEnumCommand<BombIcon>(provider, BombPerk.STAT_ID, BombIcon.DEFAULT,
-    "Bomb Icon", true) {
-  override protected void openMenu(PlayerWrapper player, BombIcon data,
-    BombIcon equipped) {
-    var bombPerkData =
-      new BombPerkData { Equipped = equipped, Unlocked = data };
-    var menu = new BombIconMenu(Provider, bombPerkData);
-    Menus.OpenMenu(player, menu);
+public class BombIconCommand(IServiceProvider provider) : ICommand {
+  private readonly ICommandManager commands =
+    provider.GetRequiredService<ICommandManager>();
+
+  private readonly IEcoManager eco = provider.GetRequiredService<IEcoManager>();
+
+  private readonly IGangChatPerk? gangChat =
+    provider.GetService<IGangChatPerk>();
+
+  private readonly IGangManager gangs =
+    provider.GetRequiredService<IGangManager>();
+
+  private readonly IGangStatManager gangStats =
+    provider.GetRequiredService<IGangStatManager>();
+
+  private readonly IStringLocalizer localizer =
+    provider.GetRequiredService<IStringLocalizer>();
+
+  private readonly IMenuManager menus =
+    provider.GetRequiredService<IMenuManager>();
+
+  private readonly IPlayerManager players =
+    provider.GetRequiredService<IPlayerManager>();
+
+  private readonly IRankManager ranks =
+    provider.GetRequiredService<IRankManager>();
+
+  public string Name => "css_bombicon";
+  public string[] Usage => ["<icon>"];
+
+  public void Start() { commands.RegisterCommand(this); }
+
+  public async Task<CommandResult> Execute(PlayerWrapper? executor,
+    CommandInfoWrapper info) {
+    if (executor == null) return CommandResult.PLAYER_ONLY;
+    var player = await players.GetPlayer(executor.Steam)
+      ?? throw new PlayerNotFoundException(executor.Steam);
+    if (player.GangId == null) {
+      info.ReplySync(localizer.Get(MSG.NOT_IN_GANG));
+      return CommandResult.SUCCESS;
+    }
+
+    var gang = await gangs.GetGang(player.GangId.Value)
+      ?? throw new GangNotFoundException(player.GangId.Value);
+
+    var (success, data) =
+      await gangStats.GetForGang<BombPerkData>(gang, BombPerk.STAT_ID);
+
+    if (!success || data == null) data = new BombPerkData();
+
+    if (info.ArgCount == 1) {
+      var menu = new BombIconMenu(provider, data);
+      await menus.OpenMenu(executor, menu);
+      return CommandResult.SUCCESS;
+    }
+
+    BombIcon icon;
+    var      query = string.Join('_', info.Args.Skip(1)).ToUpper();
+    if (!int.TryParse(info[1], out var iconInt) || iconInt < 0) {
+      if (!Enum.TryParse(query, out icon)) {
+        info.ReplySync(localizer.Get(MSG.COMMAND_INVALID_PARAM, info[1],
+          "an icon"));
+        return CommandResult.SUCCESS;
+      }
+    } else { icon = (BombIcon)iconInt; }
+
+    if (!data.Unlocked.HasFlag(icon)) {
+      var (canPurchase, minRank) = await ranks.CheckRank(player,
+        Perm.PURCHASE_PERKS);
+
+      if (!canPurchase) {
+        info.ReplySync(localizer.Get(MSG.GENERIC_NOPERM_RANK, minRank.Name));
+        return CommandResult.SUCCESS;
+      }
+
+      var cost = icon.GetCost();
+      if (await eco.TryPurchase(executor, cost,
+        item: "Bomb Icon: " + icon.ToString().ToTitleCase()) < 0)
+        return CommandResult.SUCCESS;
+
+      data.Unlocked |= icon;
+      data.Equipped =  icon;
+
+      await gangStats.SetForGang(gang, BombPerk.STAT_ID, data);
+
+      if (gangChat == null) return CommandResult.SUCCESS;
+
+      await gangChat.SendGangChat(player, gang,
+        localizer.Get(MSG.PERK_PURCHASED, icon.ToString()));
+      return CommandResult.SUCCESS;
+    }
+
+    if (data.Equipped == icon) return CommandResult.SUCCESS;
+
+    var (canManage, required) =
+      await ranks.CheckRank(player, Perm.MANAGE_PERKS);
+    if (!canManage) {
+      info.ReplySync(localizer.Get(MSG.GENERIC_NOPERM_RANK, required.Name));
+      return CommandResult.SUCCESS;
+    }
+
+    data.Equipped = icon;
+    await gangStats.SetForGang(gang, BombPerk.STAT_ID, data);
+
+    if (gangChat == null) return CommandResult.SUCCESS;
+
+    await gangChat.SendGangChat(player, gang,
+      localizer.Get(MSG.GANG_THING_SET, "Bomb Icon",
+        icon.ToString().ToTitleCase()));
+    return CommandResult.SUCCESS;
   }
-
-  public override string Name => "css_bombicon";
-
-  override protected int getCost(BombIcon item) { return item.GetCost(); }
 }
