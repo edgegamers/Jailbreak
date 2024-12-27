dcda1e9ecbb1519bf093358ea85a22bf3b3b854d
MSWS <imodmaker@gmail.com>
2024-12-26 16:34:24 -0800

    Cleanup rebel

--- public/Jailbreak.Public/Mod/LastRequest/ILastRequestRebelManager.cs
+++ public/Jailbreak.Public/Mod/LastRequest/ILastRequestRebelManager.cs
@@ -6,9 +6,14 @@ namespace Jailbreak.Public.Mod.LastRequest;
 public interface ILastRequestRebelManager : IPluginBehavior {
   public HashSet<int> PlayersLRRebelling { get; }
   public void StartLRRebelling(CCSPlayerController player);
-  public bool IsInLRRebelling(int playerSlot);
-  public void AddLRRebelling(int playerSlot);
-  public void ClearLRRebelling();
-  public int DetermineFinalRebelHealth(CCSPlayerController player);
-  public int CalculateRebelHealthRatio();
+
+  public bool IsInLRRebelling(int playerSlot) {
+    return PlayersLRRebelling.Contains(playerSlot);
+  }
+
+  public void AddLRRebelling(int playerSlot) {
+    PlayersLRRebelling.Add(playerSlot);
+  }
+
+  public void ClearLRRebelling() { PlayersLRRebelling.Clear(); }
 }
