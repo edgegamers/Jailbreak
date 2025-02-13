09d5cd91294683404549153a18c2638d7f13db29
MSWS <imodmaker@gmail.com>
2025-02-13 09:47:59 -0800

    Fix LR crashes

--- mod/Jailbreak.LastRequest/LastRequestRebelCommand.cs
+++ mod/Jailbreak.LastRequest/LastRequestRebelCommand.cs
@@ -1,6 +1,8 @@
-﻿using CounterStrikeSharp.API.Core;
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
 using CounterStrikeSharp.API.Modules.Commands;
+using CounterStrikeSharp.API.Modules.Memory;
 using CounterStrikeSharp.API.Modules.Utils;
 using Jailbreak.Formatting.Extensions;
 using Jailbreak.Formatting.Views.LastRequest;
@@ -13,6 +15,30 @@ namespace Jailbreak.LastRequest;
 public class LastRequestRebelCommand(ILastRequestManager lastRequestManager,
   ILastRequestRebelManager lastRequestRebelManager, ILRLocale messages)
   : IPluginBehavior {
+  private readonly Dictionary<int, int> rebellerHealths = [];
+
+  public void Start(BasePlugin basePlugin) {
+    basePlugin.RegisterListener<Listeners.OnEntityParentChanged>(OnDrop);
+  }
+
+  private void OnDrop(CEntityInstance entity, CEntityInstance newparent) {
+    if (!entity.IsValid || !Tag.WEAPONS.Contains(entity.DesignerName)) return;
+
+    var weapon = Utilities.GetEntityFromIndex<CCSWeaponBase>((int)entity.Index);
+    if (weapon == null
+      || weapon.PrevOwner.Get()?.OriginalController.Get() == null)
+      return;
+
+    var owner = weapon.PrevOwner.Get()?.OriginalController.Get();
+    if (owner == null || newparent.IsValid) return;
+
+    if (!rebellerHealths.TryGetValue(owner.Slot, out var hp)) return;
+    if (owner.Pawn.Value != null)
+      owner.SetHealth(Math.Min(hp, owner.Pawn.Value.Health));
+
+    rebellerHealths.Remove(owner.Slot);
+  }
+
   [ConsoleCommand("css_rebel", "Rebel during last request as a prisoner")]
   [CommandHelper(whoCanExecute: CommandUsage.CLIENT_ONLY)]
   public void Command_Rebel(CCSPlayerController? rebeller, CommandInfo info) {
@@ -38,12 +64,15 @@ public class LastRequestRebelCommand(ILastRequestManager lastRequestManager,
       return;
     }
 
+    if (rebeller.Pawn.Value != null)
+      rebellerHealths[rebeller.Slot] = rebeller.Pawn.Value.Health;
     lastRequestRebelManager.StartLRRebelling(rebeller);
   }
 
   [GameEventHandler]
   public HookResult OnRoundEnd(EventRoundEnd @event, GameEventInfo info) {
     lastRequestRebelManager.ClearLRRebelling();
+    rebellerHealths.Clear();
     return HookResult.Continue;
   }
 }
