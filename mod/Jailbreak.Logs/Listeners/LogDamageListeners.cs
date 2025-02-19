9351c38efbd94fce565a75c7e12f633abd008fa9
blankdvth <contact@blankdvth.com>
2025-02-18 22:22:48 -0500

    fix(logs): properly log player kills/deaths

--- mod/Jailbreak.Logs/Listeners/LogDamageListeners.cs
+++ mod/Jailbreak.Logs/Listeners/LogDamageListeners.cs
@@ -33,11 +33,8 @@ public class LogDamageListeners : IPluginBehavior {
     var health  = @event.DmgHealth;
 
     if (isWorld) {
-      if (health > 0)
-        logs.Append("The world hurt", logs.Player(player),
-          $"for {health} damage");
-      else
-        logs.Append("The world killed", logs.Player(player));
+      logs.Append("The world hurt", logs.Player(player),
+        $"for {health} damage");
     } else {
       if (attacker == null) {
         logs.Append(logs.Player(player), "was hurt by an unknown source",
@@ -45,13 +42,31 @@ public class LogDamageListeners : IPluginBehavior {
         return HookResult.Continue;
       }
 
-      if (health > 0)
-        logs.Append(logs.Player(attacker), "hurt", logs.Player(player),
-          $"for {health} damage");
-      else
-        logs.Append(logs.Player(attacker), "killed", logs.Player(player));
+      logs.Append(logs.Player(attacker), "hurt", logs.Player(player),
+        $"for {health} damage");
     }
 
+    return HookResult.Continue;
+  }
+
+  [GameEventHandler]
+  public HookResult OnPlayerDeath(EventPlayerDeath @event, GameEventInfo info) {
+    var player = @event.Userid;
+    if (player == null || !player.IsReal()) return HookResult.Continue;
+    var attacker = @event.Attacker;
+    
+    var isWorld = attacker == null || !attacker.IsReal();
+
+    if (isWorld) {
+      logs.Append("The world killed", logs.Player(player));
+    } else {
+      if (attacker == null) {
+        logs.Append(logs.Player(player), "was killed by an unknown source");
+        return HookResult.Continue;
+      }
+      logs.Append(logs.Player(attacker), "killed", logs.Player(player));
+    }
+    
     return HookResult.Continue;
   }
 }
