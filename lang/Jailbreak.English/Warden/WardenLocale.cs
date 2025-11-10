5750e725c3a03a7638151b89b55a6e3af33f878e
Lucca Bonatto <devbonatto@gmail.com>
2025-11-10 18:15:52 -0300

    :sparkles: feat: adding portuguese translations

--- lang/Jailbreak.English/Warden/WardenLocale.cs
+++ lang/Jailbreak.English/Warden/WardenLocale.cs
@@ -11,88 +11,88 @@ namespace Jailbreak.English.Warden;
 public class WardenLocale : IWardenLocale,
   ILanguage<Formatting.Languages.English> {
   public static readonly FormatObject PREFIX =
-    new HiddenFormatObject($" {ChatColors.DarkBlue}Guard>") {
-      //	Hide in panorama and center text
+    new HiddenFormatObject($" {ChatColors.DarkBlue}Guarda>") {
+      //  Hide in panorama and center text
       Plain = false, Panorama = false, Chat = true
     };
 
   public static readonly FormatObject COMMAND_STANDS =
-    new HiddenFormatObject($"The previous command stands for 10 seconds.");
+    new HiddenFormatObject($"O comando anterior vale por 10 segundos.");
 
   public IView PickingShortly
     => new SimpleView {
       PREFIX,
-      $"Picking a warden shortly, type {ChatColors.BlueGrey}!warden{ChatColors.Grey} to enter the queue."
+      $"Escolhendo um guarda em breve, digite {ChatColors.BlueGrey}!warden{ChatColors.Grey} para entrar na fila."
     };
 
   public IView NoWardens
     => new SimpleView {
       PREFIX,
-      $"No one in queue. Next guard to {ChatColors.BlueGrey}!warden{ChatColors.Grey} will become warden."
+      $"Ninguém na fila. O próximo guarda a {ChatColors.BlueGrey}!warden{ChatColors.Grey} será o guarda."
     };
 
   public IView NowFreeday
     => new SimpleView {
       PREFIX,
-      $"It is now a freeday! CTs must pursue {ChatColors.BlueGrey}!warden{ChatColors.Grey}."
+      $"Agora é freeday! CTs devem ir atrás do {ChatColors.BlueGrey}!warden{ChatColors.Grey}."
     };
 
   public IView WardenLeft
-    => new SimpleView { PREFIX, "The warden left the game.", COMMAND_STANDS };
+    => new SimpleView { PREFIX, "O guarda saiu do jogo.", COMMAND_STANDS };
 
   public IView WardenDied
     => new SimpleView {
       {
         PREFIX,
-        $"The warden {ChatColors.Red}died{ChatColors.Grey}. It is a freeday!"
+        $"O guarda {ChatColors.Red}morreu{ChatColors.Grey}. Agora é freeday!"
       },
       SimpleView.NEWLINE, {
         PREFIX,
-        $"CTs must pursue {ChatColors.BlueGrey}!warden{ChatColors.Grey}."
+        $"CTs devem ir atrás do {ChatColors.BlueGrey}!warden{ChatColors.Grey}."
       }
     };
 
   public IView BecomeNextWarden
     => new SimpleView {
       PREFIX,
-      $"Type {ChatColors.BlueGrey}!warden{ChatColors.Grey} to become the warden."
+      $"Digite {ChatColors.BlueGrey}!warden{ChatColors.Grey} para se tornar o guarda."
     };
 
   public IView JoinRaffle
     => new SimpleView {
       PREFIX,
-      $"You {ChatColors.White}joined {ChatColors.Grey}the warden raffle."
+      $"Você {ChatColors.White}entrou {ChatColors.Grey}no sorteio para guarda."
     };
 
   public IView LeaveRaffle
     => new SimpleView {
-      PREFIX, $"You {ChatColors.Red}left {ChatColors.Grey}the warden raffle."
+      PREFIX, $"Você {ChatColors.Red}saiu {ChatColors.Grey}do sorteio para guarda."
     };
 
   public IView NotWarden
     => new SimpleView {
-      PREFIX, $"{ChatColors.LightRed}You are not the warden."
+      PREFIX, $"{ChatColors.LightRed}Você não é o guarda."
     };
 
   public IView FireCommandFailed
     => new SimpleView {
-      PREFIX, "The fire command failed for some unknown reason..."
+      PREFIX, "O comando para demitir falhou por um motivo desconhecido..."
     };
   
   public IView TogglingNotEnabled
     => new SimpleView {
-      PREFIX, "Toggling Auto-Warden is not supported on this server."
+      PREFIX, "Ativar/Desativar Auto-Guarda não é suportado neste servidor."
     };
 
   public IView PassWarden(CCSPlayerController player) {
     return new SimpleView {
-      PREFIX, player, "resigned from warden.", COMMAND_STANDS
+      PREFIX, player, "renunciou ao cargo de guarda.", COMMAND_STANDS
     };
   }
 
   public IView FireWarden(CCSPlayerController player) {
     return new SimpleView {
-      PREFIX, player, "was fired from warden.", COMMAND_STANDS
+      PREFIX, player, "foi demitido do cargo de guarda.", COMMAND_STANDS
     };
   }
 
@@ -101,50 +101,50 @@ public class WardenLocale : IWardenLocale,
     return new SimpleView {
       PREFIX,
       admin,
-      "fired",
+      "demitiu",
       player,
-      "from warden.",
+      "do cargo de guarda.",
       COMMAND_STANDS
     };
   }
 
   public IView NewWarden(CCSPlayerController player) {
-    return new SimpleView { PREFIX, player, "is now the warden." };
+    return new SimpleView { PREFIX, player, "é o novo guarda." };
   }
   
   public IView CurrentWarden(CCSPlayerController? player) {
     return player is not null ?
-      new SimpleView { PREFIX, "The warden is", player, "." } :
-      new SimpleView { PREFIX, "There is no warden." };
+      new SimpleView { PREFIX, "O guarda é", player, "." } :
+      new SimpleView { PREFIX, "Não há nenhum guarda." };
   }
 
   public IView CannotWardenDuringWarmup
-    => new SimpleView { PREFIX, "You cannot warden during warmup." };
+    => new SimpleView { PREFIX, "Você não pode ser guarda durante o aquecimento." };
 
   public IView FireCommandSuccess(CCSPlayerController player) {
     return new SimpleView {
-      PREFIX, player, "was fired and is no longer the warden."
+      PREFIX, player, "foi demitido e não é mais o guarda."
     };
   }
 
   public IView MarkerPlaced(string marker) {
     return new SimpleView {
-      PREFIX, $"{marker}{ChatColors.Grey} marker placed."
+      PREFIX, $"{marker}{ChatColors.Grey} marcador colocado."
     };
   }
 
   public IView MarkerRemoved(string marker) {
     return new SimpleView {
-      PREFIX, $"{marker}{ChatColors.Grey} marker removed."
+      PREFIX, $"{marker}{ChatColors.Grey} marcador removido."
     };
   }
 
   public IView AutoWardenToggled(bool enabled) {
     return new SimpleView {
       PREFIX,
-      ChatColors.Grey + "You",
-      enabled ? ChatColors.Green + "enabled" : ChatColors.Red + "disabled",
-      ChatColors.Grey + "Auto-Warden."
+      ChatColors.Grey + "Você",
+      enabled ? ChatColors.Green + "ativou" : ChatColors.Red + "desativou",
+      ChatColors.Grey + " o Auto-Guarda."
     };
   }
 }
