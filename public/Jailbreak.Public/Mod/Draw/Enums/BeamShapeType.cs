25fa21bf555b2e329c7b3005d1c589d452a42cb4
ShookEagle <shookeagle@gmail.com>
2025-12-13 15:40:33 -0600

    Create BeamShapeType Enum

---
+++ public/Jailbreak.Public/Mod/Draw/Enums/BeamShapeType.cs
@@ -0,0 +1,16 @@
+﻿namespace Jailbreak.Public.Mod.Draw.Enums;
+
+/// <summary>
+///   Represents the type of beam, shape that can be drawn in 3D space using beams.
+/// </summary>
+public enum BeamShapeType {
+  CIRCLE,
+  SQUARE,
+  DIAMOND,
+  STAR,
+  TRIANGLE,
+  PENTAGON,
+  HEXAGON,
+  HEART,
+  AMONG_US
+}
