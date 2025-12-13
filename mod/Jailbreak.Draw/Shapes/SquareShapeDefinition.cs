31f84de00e8e55cdffe32da45ea2f8e57d2c1810
ShookEagle <shookeagle@gmail.com>
2025-12-13 15:42:52 -0600

    Define Square and Diamond

---
+++ mod/Jailbreak.Draw/Shapes/SquareShapeDefinition.cs
@@ -0,0 +1,21 @@
+﻿using Jailbreak.Public.Mod.Draw;
+using Jailbreak.Public.Mod.Draw.Enums;
+
+namespace Jailbreak.Draw.Shapes;
+
+/// <summary>
+///   Square shape definition.
+///   Desmos visualization: https://www.desmos.com/calculator/lirekvc8ku
+/// </summary>
+public class SquareShapeDefinition : IBeamShapeDefinition {
+  // Pre-baked square (unit size)
+  private static readonly (float x, float y)[] SQUARE_POINTS = [
+    (1.0f, 1.0f), (-1.0f, 1.0f), (-1.0f, -1.0f), (1.0f, -1.0f)
+  ];
+
+  public BeamShapeType ShapeType => BeamShapeType.SQUARE;
+  public IReadOnlyList<(float x, float y)> UnitPoints => SQUARE_POINTS;
+  public bool IsClosed => true;
+  public float DefaultRadius => 30f;
+  public float DefaultWidth => 1.5f;
+}
